51417c81130ce27206b1309e8cb31a97ff314eb8
chenxu5 <chenxu5@beisen.com>
2024-06-08 01:26:39 +0800

    br

--- src/components/YearsStat/index.tsx
+++ src/components/YearsStat/index.tsx
@@ -18,11 +18,11 @@ const YearsStat = ({ year, onClick }: { year: string, onClick: (_year: string) =
           {INFO_MESSAGE(years.length, year)}
           <br />
           <br />
-          “骑行是一种生活方式，<br>
-          一种习惯，一种瘾。<br>
-          钟情于骑行在路上的状态，<br>
-          自由、未知、期待、惊奇，<br>
-          可以获得，也可以遗忘。”<br>
+          “骑行是一种生活方式，<br />
+          一种习惯，一种瘾。<br />
+          钟情于骑行在路上的状态，<br />
+          自由、未知、期待、惊奇，<br />
+          可以获得，也可以遗忘。”<br />
           &nbsp;&nbsp;&nbsp;&nbsp;––ChenDaqian
         </p>
       </section>
